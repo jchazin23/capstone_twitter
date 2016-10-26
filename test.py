efe297ba10ecf6bb9954d3e2200f6252c62da39d
jchazin23 <jchazin@gmail.com>
2016-10-25 22:58:39 -0400

    testing basic mongo connectivity with pymongo  Please enter the commit message for your changes. Lines starting

--- test.py
+++ test.py
@@ -1,2 +1,12 @@
+## this should be run once connected to the aws instance
+
+
 from twython import Twython
 from pymongo import MongoClient
+
+client = MongoClient()
+db = client.twitter_final
+col = db.tweets
+
+col.find_one() ## this returns one tweet
+
